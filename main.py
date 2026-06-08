faf4f9ee51c0035beab8d036f45e37eb70a2b43d
V P <akash.v-p@capgemini.com>
2026-06-08 10:17:06 +0530

    how to print random num

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ with open("sample.txt") as file:
 content_capitalized = content.upper();
 print(content_capitalized);
 print("Checking revert command in git");
+
+print("how do i print random humber");
+import random
+print(random.randint(1, 100));

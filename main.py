0577a8ec13b580fc215458a7259fb154af556cdf
V P <akash.v-p@capgemini.com>
2026-06-11 10:50:02 +0530

    try and catch block added

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-with open("sample.txt") as file:
+try:
+    with open("sample.txt") as file:
     content = file.read();
 
-with open('user_file.txt','w') as file:
+    with open('user_file.txt','w') as file:
     file.write('user content');
 
-print('File content:');
-print(content); 
-print('Done');
+    print('File content:');
+    print(content); 
+    print('Done');
+
+except FileNotFoundError:
+    print('File not found');
+except IOError:
+    print('IO error occurred');
+
 
 
 
